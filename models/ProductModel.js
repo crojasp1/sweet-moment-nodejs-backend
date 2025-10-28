a4011fd8c96a80ebe9197a0141875632803874da
ENERIED PRIETO SAENZ <camilorojasprieto@gmail.com>
2025-10-28 06:23:30 -0500

    Correccion del modelo

--- models/ProductModel.js
+++ models/ProductModel.js
@@ -4,12 +4,13 @@ const Schema = mongoose.Schema;
 
 const ProductSchema = new Schema(
   {
-    nombre: {type: String},
-    descripcion: {type:String},
-    precio: {type: Intl},
-    stock: {type:Intl}
-  }, 
-  {collection: 'productos'}
+    name: { type: String, required: true },
+    image: { type: String },
+    category: { type: String },
+    old_price: { type: Number },
+    new_price: { type: Number },
+  },
+  { collection: 'productos' }
 );
 
 export default mongoose.model('ProductModel', ProductSchema);
